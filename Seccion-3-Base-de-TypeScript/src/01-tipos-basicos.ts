a4b366a28640aa26c00b6fe654d9ad17b068a5aa
AlexisleoMena <alexismena2690@gmail.com>
2023-01-21 22:15:09 -0300

    Seccion-3-Base-de-TypeScript 01-tipos-basicos

---
+++ Seccion-3-Base-de-TypeScript/src/01-tipos-basicos.ts
@@ -0,0 +1,21 @@
+let nombre: string;
+nombre = "Alexis";
+
+let apellido: string = "Mena";
+let edad: number = 2;
+let estaSoltero: boolean = true;
+let CantidadDeDinero: null = null;
+let destinoTuristico: undefined = undefined;
+let claveInstagram: string | number = "hola123";
+let claveGmail: string | number | boolean = 123;
+
+// unknown no permite acceder a propiedades arbitrarias, estas variables deben estar
+// dentro de condicionales que aseguren que el tipo de dato almacenado le corresponde tal propiedad.
+let res: unknown = ['Alexis', 'Mena'];
+Array.isArray(res) && res.sort();
+
+//any permite acceder a propiedades arbitrarias, incluso no inexistentes.
+let claveFacebook: any = "hola123";
+claveFacebook.toUpperCase();
+
+
